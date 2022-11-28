18af5ed1543ca2c840d358e1a7c3d1b5452db364
mtlthu <mtlthu1991@gmail.com>
2022-11-28 16:29:36 +0700

    Bai 65 Xây Dựng UI Phần Header Dự Án Tiktok

---
+++ src/components/Layout/AccountItem/index.js
@@ -0,0 +1,27 @@
+import styles from './AccountItem.module.scss';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx('wrapper')}>
+      <img
+        className={cx('avatar')}
+        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/f122438e807e772a4d4fbb3e9092843c~c5_720x720.jpeg?x-expires=1669777200&x-signature=yi%2BHkqgh2%2FIPIf42cm5c51QBENA%3D"
+        alt="Hoaa"
+      />
+      <div className={cx('info')}>
+        <h4 className={cx('name')}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx('username')}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,28 +1,66 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
-import { faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faSignIn, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+import Button from '~/components/Button';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; // optional
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '../../AccountItem';
 
 const cx = classNames.bind(styles);
 
 function Header() {
+  const [searchResult, setSearchResult] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([]);
+    }, 3000);
+  }, []);
+
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
         <div className={cx('logo')}>
           <img src={images.logo} alt="TikTok" />
         </div>
-        <div className={cx('search')}>
-          <input placeholder="Search accounts and videos" spellCheck={false} />
-          <button className={cx('clear')}>
-            <FontAwesomeIcon icon={faCircleXmark} />
-          </button>
-          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-          <button className={cx('search-btn')}>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
+        <Tippy
+          interactive={true}
+          visible={searchResult.length > 0}
+          render={(attrs) => (
+            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx('search-title')}>accounts</h4>
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+              </PopperWrapper>
+            </div>
+          )}
+        >
+          <div className={cx('search')}>
+            <input placeholder="Search accounts and videos" spellCheck={false} />
+            <button className={cx('clear')}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+            <button className={cx('search-btn')}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </Tippy>
+        <div className={cx('actions')}>
+          <Button rounded className={cx('custom-login')} large>
+            Upload
+          </Button>
+          <Button primary leftIcon={<FontAwesomeIcon icon={faSignIn} />} large>
+            Log in
+          </Button>
         </div>
       </div>
     </header>
