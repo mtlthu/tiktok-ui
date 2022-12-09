f32dbef2008a7a608cf44ef42d397731020302e6
mtlthu <mtlthu1991@gmail.com>
2022-12-09 11:49:50 +0700

    Bai 75 Sửa Lỗi Và Hoàn Thiện Phần Header

--- src/components/Popper/Wrapper.js
+++ src/components/Popper/Wrapper.js
@@ -4,7 +4,6 @@ import styles from './Popper.module.scss';
 const cx = classNames.bind(styles);
 
 function Wrapper({ children, className }) {
-  console.log('children: ', children);
   return <div className={cx('wrapper', className)}>{children}</div>;
 }
 
