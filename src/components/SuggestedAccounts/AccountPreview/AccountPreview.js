83690303769321b6b250777afa8b8a0eb47d9fbe
mtlthu <mtlthu1991@gmail.com>
2022-12-14 12:20:42 +0700

    Bai 83: Xây dựng UI phần sidebar

---
+++ src/components/SuggestedAccounts/AccountPreview/AccountPreview.js
@@ -0,0 +1,38 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from '~/components/Button';
+import styles from './AccountPreview.module.scss';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+const cx = classNames.bind(styles);
+
+function AccountPreview() {
+  return (
+    <div className={cx('wrapper')}>
+      <div className={cx('header')}>
+        <img
+          className={cx('avatar')}
+          src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/65d3c6b1d1e205c75536ccf1f26d552d~c5_100x100.jpeg?x-expires=1671087600&amp;x-signature=WCQJ6SdfGul0h%2Bw2uf%2FmM2FpoeY%3D"
+          alt=""
+        />
+        <Button primary className={cx('follow-btn')}>
+          Follow
+        </Button>
+      </div>
+      <div className={cx('body')}>
+        <h4 className={cx('nickname')}>
+          <strong>quocnguyenphu</strong>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <p className={cx('name')}>quoc nguyen phu</p>
+        <p className={cx('analytics')}>
+          <strong className={cx('value')}>8.2M </strong>
+          <span className={cx('lable')}>Followers </span>
+          <strong className={cx('value')}>8.2M </strong>
+          <span className={cx('lable')}>LIKES </span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default AccountPreview;
